9ddb2a8e548eb8461acde5a3bc56ccb1414c2173
Adam Porter <adam@alphapapa.net>
2015-11-16 15:30:21 -0600

    org-protocol-capture-html.el: Update bookmarklet in comment
    Also minor formatting adjustments

--- org-protocol-capture-html.el
+++ org-protocol-capture-html.el
@@ -3,13 +3,10 @@
 ;;; Commentary:
 ;; This makes it possible to capture HTML into Org-mode with
 ;; org-protocol by passing it through Pandoc to convert the HTML into
-;; Org syntax. You can use a JavaScript function like the ones found
-;; here[0] to get the HTML from the browser's selection, or here's one
-;; that seems to work:
+;; Org syntax. You can use a JavaScript bookmarklet to get the HTML
+;; from the browser's selection, such as this:
 ;;
-;; function () {var html = ""; if (typeof window.getSelection != "undefined") {var sel = window.getSelection(); if (sel.rangeCount) {var container = document.createElement("div"); for (var i = 0, len = sel.rangeCount; i < len; ++i) {container.appendChild(sel.getRangeAt(i).cloneContents());} html = container.innerHTML;}} else if (typeof document.selection != "undefined") {if (document.selection.type == "Text") {html = document.selection.createRange().htmlText;}} return html;}();
-;;
-;; [0] http://stackoverflow.com/a/6668159/712624
+;; content.location.href = 'org-protocol://capture-html://w/' + encodeURIComponent(content.location.href) + '/' + encodeURIComponent(content.document.title) + '/' + encodeURIComponent(function () {var html = ""; if (typeof content.document.getSelection != "undefined") {var sel = content.document.getSelection(); if (sel.rangeCount) {var container = document.createElement("div"); for (var i = 0, len = sel.rangeCount; i < len; ++i) {container.appendChild(sel.getRangeAt(i).cloneContents());} html = container.innerHTML;}} else if (typeof document.selection != "undefined") {if (document.selection.type == "Text") {html = document.selection.createRange().htmlText;}} var relToAbs = function (href) {var a = content.document.createElement("a"); a.href = href; var abs = a.protocol + "//" + a.host + a.pathname + a.search + a.hash; a.remove(); return abs;}; var elementTypes = [['a', 'href'], ['img', 'src']]; var div = content.document.createElement('div'); div.innerHTML = html; elementTypes.map(function(elementType) {var elements = div.getElementsByTagName(elementType[0]); for (var i = 0; i < elements.length; i++) {elements[i].setAttribute(elementType[1], relToAbs(elements[i].getAttribute(elementType[1])));}}); return div.innerHTML;}());
 
 ;;; Code:
 (defun org-protocol-capture-html-with-pandoc (data)
@@ -18,11 +15,13 @@
 This function is basically a copy of `org-protocol-do-capture', but
 it passes the captured content (not the URL or title) through
 Pandoc, converting HTML to Org-mode."
+
   ;; It would be nice to not basically duplicate
   ;; `org-protocol-do-capture', but passing the data back to that
   ;; function would require re-encoding the data into a URL string
   ;; with Emacs after Pandoc converts it.  Since we've already split
   ;; it up, we might as well go ahead and run the capture directly.
+
   (let* ((parts (org-protocol-split-data data t org-protocol-data-separator))
 	 (template (or (and (>= 2 (length (car parts))) (pop parts))
 		       org-protocol-default-template-key))
@@ -34,7 +33,7 @@ Pandoc, converting HTML to Org-mode."
 	 (orglink (org-make-link-string
 		   url (if (string-match "[^[:space:]]" title) title url)))
 	 (query (or (org-protocol-convert-query-to-plist (cadddr parts)) ""))
-	 (org-capture-link-is-already-stored t)) ;; avoid call to org-store-link
+	 (org-capture-link-is-already-stored t)) ; avoid call to org-store-link
 
     (setq org-stored-links
           (cons (list url title) org-stored-links))
@@ -58,10 +57,11 @@ Pandoc, converting HTML to Org-mode."
 
         ;; Demote page headings in capture buffer to below the
         ;; top-level Org heading
-        (save-excursion (goto-char (point-min))
-                        (re-search-forward (rx bol "*" (1+ space)) nil t) ; Skip 1st heading
-                        (while (re-search-forward (rx bol "*" (1+ space)) nil t)
-                          (org-demote-subtree)))))
+        (save-excursion
+          (goto-char (point-min))
+          (re-search-forward (rx bol "*" (1+ space)) nil t) ; Skip 1st heading
+          (while (re-search-forward (rx bol "*" (1+ space)) nil t)
+            (org-demote-subtree)))))
     nil))
 
 (add-to-list 'org-protocol-protocol-alist
