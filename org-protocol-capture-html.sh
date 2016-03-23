2492d56fda43238a9bf021f19e400428cf72b720
Adam Porter <adam@alphapapa.net>
2016-03-23 07:59:44 -0500

    org-protocol-capture-html.sh: Add URL download to script
    This way, you can simply pass it a URL and it will download and capture
    the page.  Of course, this will include the entire page content, so you
    might want to trim off headers and footers.
    
    Also, reorganize the script according to my template, adding debug
    function, etc.

--- org-protocol-capture-html.sh
+++ org-protocol-capture-html.sh
@@ -1,69 +1,147 @@
 #!/bin/bash
 
 # * Defaults
+
 heading="Heading"
 template="w"
-url="http://example.com"
 
 # * Functions
-function urlencode {
-    python -c "import sys, urllib; print urllib.quote(sys.stdin.read(), safe='')"
+
+function debug {
+    if [[ -n $debug ]]
+    then
+        function debug {
+            echo "DEBUG: $@" >&2
+        }
+        debug "$@"
+    else
+        function debug {
+            true
+        }
+    fi
+}
+function die {
+    echo "$@" >&2
+    exit 1
 }
 function usage {
     cat <<EOF
-org-protocol-capture-html [-h HEADING] [-t TEMPLATE] [-u URL] [HTML]
+$0 [OPTIONS] [HTML]
+html | $0 [OPTIONS]
 
 Send HTML to Emacs through org-protocol, passing it through Pandoc to
 convert HTML to Org-mode.  HTML may be passed as an argument or
-through STDIN.
+through STDIN.  If only URL is given, it will be downloaded and its
+contents used.
 
 Options:
-    -h HEADING   Use HEADING as the Org heading
-    -t TEMPLATE  Use the org-capture template with TEMPLATE key (default: w)
-    -u URL       Use URL for the heading link
+    -h, --heading HEADING     Heading
+    -t, --template TEMPLATE   org-capture template key (default: w)
+    -u, --url URL             URL
+
+    --debug  Print debug info
+    --help   I need somebody!
 EOF
 }
 
+function urlencode {
+    python -c "import sys, urllib; print urllib.quote(sys.stdin.read(), safe='')"
+}
+
 # * Args
-while getopts "h:t:u:" opt
+
+args=$(getopt -n "$0" -o dh:t:u: -l debug,help,heading:,template:,url: -- "$@") || exit 1
+eval set -- "$args"
+
+while true
 do
-    case $opt in
-        h) heading=$OPTARG ;;
-        t) template=$OPTARG ;;
-        u) url=$OPTARG ;;
-        *) usage; exit ;;
+    case "$1" in
+        -d|--debug)
+            debug=true
+            debug "Debugging on"
+            ;;
+        --help)
+            usage
+            exit
+            ;;
+        -h|--heading)
+            shift
+            heading="$1"
+            ;;
+        -t|--template)
+            shift
+            template="$1"
+            ;;
+        -u|--url)
+            shift
+            url="$1"
+            ;;
+        --)
+            # Remaining args
+            shift
+            rest=("$@")
+            break
+            ;;
     esac
+
+    shift
 done
-shift $(( OPTIND - 1 ))
 
-# * Get HTML
-if [[ $@ ]]
-then
-    # Get from args
-    data="$@"
-else
-    # Get from STDIN
-    data=$(cat)
-fi
+debug "ARGS: $args"
+debug "Remaining args: ${rest[@]}"
 
-if ! [[ $data ]]
-then
-    # No data; quit
-    echo "No data passed via args or STDIN." >&2
-    exit 1
-fi
+# * Main
 
-# * Check template length
+# ** Check template
+
 if [[ ${#template} -gt 1 ]]
 then
-    echo "Template key should be one letter." >&2
-    exit 1
+    die "Template key should be one letter."
 fi
 
-# * URL-encode data
+# ** Get HTML
+
+if [[ -n $@ ]]
+then
+    debug "HTML from args"
+
+    html="$@"
+
+elif ! [[ -t 0 ]]
+then
+    debug "HTML from STDIN"
+
+    html=$(cat)
+
+elif [[ -n $url ]]
+then
+    debug "Only URL given; downloading..."
+
+    # Download URL
+    html=$(curl "$url") || die "Unable to download $url"
+
+    # Get HTML title for heading
+    heading=$(sed -nr '/<title>/{s|.*<title>([^<]+)</title>.*|\1|i;p;q};' <<<"$html") || heading="A web page with no name"
+
+    debug "Using heading: $heading"
+
+else
+    usage
+    echo
+    die "I need somethin' ta go on, Cap'n!"
+fi
+
+# ** Check URL
+# The URL shouldn't be empty
+
+[[ -n $url ]] || url="http://example.com"
+
+# ** URL-encode html
+
 heading=$(urlencode <<<"$heading")
 url=$(urlencode <<<"$url")
-data=$(urlencode <<<"$data")
+html=$(urlencode <<<"$html")
 
-# * Send to Emacs
-emacsclient "org-protocol://capture-html://$template/$url/$heading/$data"
+# ** Send to Emacs
+
+emacsclient "org-protocol://capture-html://$template/$url/$heading/$html"
